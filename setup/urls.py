549b628365e19bffbdd1ea8400f904199568cd32
Ricardo Mendonça <cadinhodev@gmail.com>
2023-12-28 22:04:21 -0300

    Update urls.py

--- setup/urls.py
+++ setup/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+def sobre(request):
+    return HttpResponse('Página sobre/')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/', sobre)
 ]
