5080f01aa7c2a1f4bcd2dd67b9b5a469498feecb
Ricardo Mendonça <cadinhodev@gmail.com>
2023-12-31 23:07:57 -0300

    att

---
+++ recipes/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(requests):
+    return render(requests, 'home.html')
+
+def contato(requests):
+    return HttpResponse('CONTATO')
+
+def sobre(requests):
+    return HttpResponse('SOBRE')
--- setup/urls.py
+++ setup/urls.py
@@ -15,15 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-
-def sobre(request):
-    return HttpResponse('Página sobre/')
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sobre/', sobre)
+    path('', include('recipes.urls'))
 ]
