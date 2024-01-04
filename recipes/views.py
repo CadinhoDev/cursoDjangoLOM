660f132e93cdc791d3bcbe9f89f5eaaeed1e54f3
Ricardo Mendonça <cadinhodev@gmail.com>
2024-01-04 10:59:33 -0300

    att

--- recipes/views.py
+++ recipes/views.py
@@ -4,10 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(requests):
-    return render(requests, 'recipes/home.html')
-
-def contato(requests):
-    return HttpResponse('CONTATO')
-
-def sobre(requests):
-    return HttpResponse('SOBRE')
+    return render(requests, 'recipes/home.html', context={
+        'name': 'Ricardo Mendonça'
+    })
